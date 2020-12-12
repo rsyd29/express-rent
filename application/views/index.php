d7004717dae93f1dfccf51ae92ceb96c751b47ee
rsyd29 <brasyid15@gmail.com>
2020-12-12 12:29:14 +0700

    UTS PWEB

---
+++ application/views/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Reserve a Car</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+	<!-- Custom fonts for this template -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<div class="d-flex justify-content-between">
+		<img class="logo" src="<?= base_url('assets/'); ?>img/logo.png" alt="" width="150px" height="100px">
+		<div class="txt-text">
+			<h1 class="text-primary">EXPRESS RENT-</h1>
+		</div>
+		<div class="login">
+			<i class="far fa-user"></i>
+			<a href="#" class="text-black">Login</a>
+			<a href="#" class="text-black">Register</a>
+		</div>
+	</div>
+
+
+	<!-- Navigation -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+		<div class="container-fluid">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">About us</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-black active" href="#">Reserve a Car</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Services</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Contact us</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<br>
+	<div class="container">
+		<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">Home</li>
+				<li class="breadcrumb-item" aria-current="page">Reserve a Car</li>
+				<li class="breadcrumb-item active" aria-current="page">Luxury Cars</li>
+			</ol>
+		</nav>
+	</div>
+
+
+	<div class="container">
+		<div class="row">
+
+			<div class="col-6 col-sm-3">
+				<div class="list-group">
+					<a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+						Featured
+					</a>
+					<a href="#" class="list-group-item list-group-item-action">Budget cars</a>
+					<a href="#" class="list-group-item list-group-item-action">General cars</a>
+					<a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Luxury Cars</a>
+					<a href="#" class="list-group-item list-group-item-action">Premium cars</a>
+					<a href="#" class="list-group-item list-group-item-action">MPV's</a>
+					<a href="#" class="list-group-item list-group-item-action">SUV's</a>
+				</div>
+			</div>
+
+			<div class="col-6 col-sm-8">
+				<div class="row">
+					<div class="col-md-4 mb-5">
+						<div class="card h-100 w-100">
+							<img class="card-img-top" src="https://europeprestigecarrent.com/wp-content/uploads/2020/03/Portofino-768x432.jpg" alt="">
+							<div class="card-body">
+								<h4 class="card-title">Ferrari</h4>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+							</div>
+							<div class="card-footer">
+								<a href="#" class="btn btn-primary">Reserve</a>
+								<a href="#" class="btn btn-primary">More</a>
+								<a href="#" class="btn btn-primary">Inquiry</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4 mb-5">
+						<div class="card h-100">
+							<img class="card-img-top" src="https://europeprestigecarrent.com/wp-content/uploads/2018/11/Phantom-768x433.jpg" alt="">
+							<div class="card-body">
+								<h4 class="card-title">Mercedes Benz</h4>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
+							</div>
+							<div class="card-footer">
+								<a href="#" class="btn btn-primary">Reserve</a>
+								<a href="#" class="btn btn-primary">More</a>
+								<a href="#" class="btn btn-primary">Inquiry</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 mb-5">
+					</div>
+					<div class="col-md-4 mb-5">
+						<div class="card h-100">
+							<img class="card-img-top" src="https://europeprestigecarrent.com/wp-content/uploads/2018/10/Van-block.jpg" alt="">
+							<div class="card-body">
+								<h4 class="card-title">Range Rover</h4>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
+							</div>
+							<div class="card-footer">
+								<a href="#" class="btn btn-primary">Reserve</a>
+								<a href="#" class="btn btn-primary">More</a>
+								<a href="#" class="btn btn-primary">Inquiry</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 mb-5">
+						<div class="card h-100">
+							<img class="card-img-top" src="https://europeprestigecarrent.com/wp-content/uploads/2018/11/model-s-768x432.jpg" alt="">
+							<div class="card-body">
+								<h4 class="card-title">Tesla</h4>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
+							</div>
+							<div class="card-footer">
+								<a href="#" class="btn btn-primary">Reserve</a>
+								<a href="#" class="btn btn-primary">More</a>
+								<a href="#" class="btn btn-primary">Inquiry</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<footer class="bg-primary footer text-faded text-center py-5">
+				<div class="container">
+					<p class="m-0 small text-white">Copyright &copy;<script>
+							document.write(new Date().getFullYear());
+						</script> expressrac.com All rights</p>
+				</div>
+			</footer>
+
+			<!-- Bootstrap core JavaScript -->
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
